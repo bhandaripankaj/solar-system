b8a6d81df64301093862e5556b93f0d4b3d0d8ed
Pankaj Bhandari <pankajbhandari@Pankajs-MacBook-Air.local>
2023-05-08 01:02:00 +0530

    worled on my solar system

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  function getRandomPosition(max) {
+    return Math.floor(Math.random() * max);
+  }
+  
+  const stars = Array.from({ length: 1000 }).map((_, i) => (
+    <div
+      key={i}
+      className="star"
+      style={{
+        top: getRandomPosition(window.innerHeight),
+        left: getRandomPosition(window.innerWidth),
+        animationDelay: `${Math.random() * 10}s`
+      }}
+    />
+  ));
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+      <div className="sun">
+      <div className="mercury"></div>
+      <div className="venus"></div>
+      <div className="earth">
+        <div className="moon"></div>  
+      </div>
+      <div className="mars"></div>
+      <div className="jupiter"></div>
+      <div className="saturn"></div>
+      <div className="uranus"></div>
+      <div className="neptune"></div>
+      </div>
+    </div>
+    {stars}
     </div>
   );
 }
